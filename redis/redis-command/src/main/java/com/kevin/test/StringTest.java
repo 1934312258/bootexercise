9247228cb8fa0b548b6ee921bb0fe7499f891380
zhaowenjian <zhaowenjian@smzdm.com>
2021-01-06 18:53:02 +0800

    redis

--- redis/redis-command/src/main/java/com/kevin/test/StringTest.java
+++ redis/redis-command/src/main/java/com/kevin/test/StringTest.java
@@ -9,6 +9,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 /**
  * @创建人 zhaowenjian
  * @创建时间 2020/12/9
@@ -17,11 +19,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StringTest {
-
-    @Autowired
+    // 必须使用@Resource注解根据bean名称装配，springboot自动注入了RedisTemplate与StringRedisTemplate两个bean，后者直接继承前者
+    // 使用autowired注解按类型装配会找到两个类型相同的bean导致注入失败，在配置文件中配置RedisTemplate是为了改变其序列化方式
+    @Resource(name = "redisTemplate")
     RedisTemplate template;
 
-    @Autowired
+    // 此处使用的是RedisTemplate内的实例变量，无需在配置文件配置，序列化方式使用的当前RedisTemplate的序列化方式
+    @Resource(name = "redisTemplate")
     static ValueOperations<String, Object> ops;
 
     @Test
